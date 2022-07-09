7f17a74a22d808cf5207ea5df2f970914b776a70
Hyeon Cheol Do <108172386+DoHyeonCheol@users.noreply.github.com>
2022-07-09 23:13:00 +0900

    초기 커밋.

---
+++ BJ15552/BJ15552.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int t;
+	int a, b;
+	cin.tie(NULL);
+
+	ios::sync_with_stdio(false);
+
+	cin >> t;
+
+	for (int i = 0; i < t; i++)
+	{
+		cin >> a >> b;
+
+		cout << a + b << '\n';
+
+	}
+}
