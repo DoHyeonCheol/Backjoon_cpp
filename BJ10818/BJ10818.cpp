20df484a1a5651b34145bdf6102449e2b9ea0732
Hyeon Cheol Do <108172386+DoHyeonCheol@users.noreply.github.com>
2022-07-29 21:43:32 +0900

    Update BJ10818.cpp

--- BJ10818/BJ10818.cpp
+++ BJ10818/BJ10818.cpp
@@ -1,4 +1,3 @@
-/*
 #include<iostream>
 
 using namespace std;
@@ -30,28 +29,4 @@ int main()
 	cout << min << " " << max << "\n";
 
 	return 0;
-}
-*/
-
-#include<iostream>
-
-using namespace std;
-
-int main()
-{
-	int N, X;
-
-	cin >> N >> X;
-
-	int arr[10000];
-
-	for (int i = 0; i < N; ++i)
-	{
-		cin >> arr[i];
-	}
-	for (int j = 0; j < N; ++j)
-		if (arr[j] < X)
-		{
-			cout << arr[j] << endl;
-		}
 }
